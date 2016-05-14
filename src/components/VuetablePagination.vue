beca015211a5ba5e2c9fab7e8b6da7783cbea33e
Rati Wannapanop <rati.wannapanop@gmail.com>
2016-05-14 05:20:11 +0000

    v1.1.0 - VuetablePaginationBootstrap is deprecated and will be removed in the next release. Use VuetablePagination with additional config instead. - VuetablePagination is now rendered as sliding pagination when there are more pages than the calculated value from `on-each-side` prop. - new option: `pagination-config` - update examples
    PaginationMixin
    - new prop: `active-class`
    - new option in `icons` props: `first`, `last`
    - new prop: `on-each-side`. Inspired by Laravel's LengthAwarePagination.
    - new computed props to support sliding pagination

--- src/components/VuetablePagination.vue
+++ src/components/VuetablePagination.vue
@@ -1,20 +1,42 @@
 <template>
-    <div class="{{wrapperClass}}">
-         <a @click="loadPage('prev')"
-             class="{{linkClass}} {{isOnFirstPage ? disabledClass : ''}}">
-                 <i :class="icons.prev"></i>
-         </a>
-         <template v-for="n in totalPage">
-             <a @click="loadPage(n+1)"
-                class="{{pageClass}} {{isCurrentPage(n+1) ? disabledClass : ''}}">
-                     {{ n+1 }}
-             </a>
-         </template>
-         <a @click="loadPage('next')"
-             class="{{linkClass}} {{isOnLastPage ? disabledClass : ''}}">
-             <i :class="icons.next"></i>
-         </a>
-    </div>
+        <div class="{{wrapperClass}}">
+            <a @click="loadPage(1)"
+                class="btn-nav {{linkClass}} {{isOnFirstPage ? disabledClass : ''}}">
+                    <i v-if="icons.first != ''" class="{{icons.first}}"></i>
+                    <span v-else>&laquo;</span>
+            </a>
+            <a @click="loadPage('prev')"
+                class="btn-nav {{linkClass}} {{isOnFirstPage ? disabledClass : ''}}">
+                    <i v-if="icons.next != ''" class="{{icons.prev}}"></i>
+                    <span v-else>&nbsp;&lsaquo;</span>
+            </a>
+            <template v-if="notEnoughPages">
+                <template v-for="n in totalPage">
+                    <a @click="loadPage(n+1)"
+                        class="{{pageClass}} {{isCurrentPage(n+1) ? activeClass : ''}}">
+                            {{ n+1 }}
+                    </a>
+                </template>
+            </template>
+            <template v-else>
+               <template v-for="n in windowSize">
+                   <a @click="loadPage(windowStart+n)"
+                        class="{{pageClass}} {{isCurrentPage(windowStart+n) ? activeClass : ''}}">
+                        {{ windowStart+n }}
+                   </a>
+               </template>
+            </template>
+            <a @click="loadPage('next')"
+                class="btn-nav {{linkClass}} {{isOnLastPage ? disabledClass : ''}}">
+                <i v-if="icons.next != ''" class="{{icons.next}}"></i>
+                <span v-else>&rsaquo;&nbsp;</span>
+            </a>
+            <a @click="loadPage(totalPage)"
+                class="btn-nav {{linkClass}} {{isOnLastPage ? disabledClass : ''}}">
+                <i v-if="icons.last != ''" class="{{icons.last}}"></i>
+                <span v-else>&raquo;</span>
+            </a>
+        </div>
 </template>
 
 <script>
